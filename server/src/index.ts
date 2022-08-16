a6119553d9bc7706ecf4d6f4683baa691d4bf799
Duc Thai <ducthai060501@gmail.com>
2022-08-16 11:33:52 +0300

    wrong dicrection, restart code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,23 @@
+import  cookieParser  from 'cookie-parser';
 import express , {Request, Response, NextFunction}from 'express'
-import { json } from 'body-parser'
-import "reflect-metadata"
-import path from 'path';
-import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-import postRouter from './routers/postRouters'
-import commentRouter from './routers/commentRouters'
-import userRouter from './routers/userRouters'
-const app = express() 
-app.use(logger('dev'));
+import path from 'path'
+import {json} from 'body-parser'
+
+
+const app = express()  ;
+
+app.use(logger('dev')) ;
 
 app.use(json()) ;
-app.use(express.urlencoded({extended: false}))
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/post', postRouter)
-app.use('/api/comment',commentRouter) 
-app.use('/api/user', userRouter)
+app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (_req: Request, res: Response) => {
     res.send('<p>Hello and welcome</p>')
 })
-
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
--- server/src/routers/postRouters.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { pool } from "../db";
-
-const router = express.Router();
-
-
-
-export default router;
